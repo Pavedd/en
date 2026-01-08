8535ea23283e66f7748354d9de515c78edc23fc0
Endersoul46 <paul.theodor.maier@gmail.com>
2026-01-08 04:25:47 +0100

    chaned some config

--- default.nix
+++ default.nix
@@ -1,23 +1,32 @@
 {
   lib,
-  rustPlatform,
+  stdenv,
   fetchFromGitHub,
+  cargo,
+  rustPlatform,
+  rustc,
 }:
 
-rustPlatform.buildRustPackage rec {
+stdenv.mkDerivation rec {
   pname = "en";
-  version = "unstable-2026-01-07";
+  version = "unstable-2026-01-08";
 
   src = fetchFromGitHub {
     owner = "Pavedd";
     repo = "en";
-    rev = "a0b08dbef827e5466637bf87190f7738842070c7";
-    hash = "sha256-zpTvER0hgjd7R/hGcpQ9H2l7hXHYKWPggjeOR4QTzQQ=";
+    rev = "6c29c2ffc23228934ebf5a19d005f8229b2c9779";
+    hash = "sha256-n8uu03hrJRNvx/ARLVliIjSEFF5o4IMKIFo4vPsRGZQ=";
   };
 
-  cargoLock = {
+  cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
   };
+
+  nativeBuildInputs = [
+    cargo
+    rustPlatform.cargoSetupHook
+    rustc
+  ];
 
   meta = {
     description = "A rust cli Helper untility for my NixOS config";
@@ -25,5 +34,6 @@ rustPlatform.buildRustPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ];
     mainProgram = "en";
+    platforms = lib.platforms.all;
   };
 }
